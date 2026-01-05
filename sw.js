46c7519ed8c9baa30f409804129a5c967b83fd28
7ng6t2snvn-lan <7ng6t2snvn@privaterelay.appleid.com>
2026-01-05 03:07:34 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'shot-tracker-2.0-medical';
+const CACHE_VERSION = 'shot-tracker-v6.1.5';
 const CACHE_FILES = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(CACHE_FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); });
