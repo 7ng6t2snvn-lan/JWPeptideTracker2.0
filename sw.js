6608281f5bd57ef077a8173005a5cbdd3894ca6b
7ng6t2snvn-lan <7ng6t2snvn@privaterelay.appleid.com>
2026-01-15 12:06:35 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'shot-tracker-v6.2.1';
+const CACHE_VERSION = 'gelato-v5.0.0-updated';
 const CACHE_FILES = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(CACHE_FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); });
