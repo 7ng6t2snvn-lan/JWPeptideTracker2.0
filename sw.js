47b035cfa87f0b48952d172b4847569bce6b225b
7ng6t2snvn-lan <7ng6t2snvn@privaterelay.appleid.com>
2026-01-15 12:15:21 -0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_VERSION = 'gelato-v5.0.0-updated';
-const CACHE_FILES = ['./index.html', './manifest.json'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(CACHE_FILES))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))))); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
